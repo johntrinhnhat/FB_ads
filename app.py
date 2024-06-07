f8d2e01fdd3a0065ffcba40ed4e723e282c240c8
KHOITRINH03 <Johntrinhnhat@gmail.com>
2024-06-07 19:29:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,21 +8,22 @@ from oauth2client.service_account import ServiceAccountCredentials
 from dotenv import load_dotenv
 from datetime import datetime
 from gspread_dataframe import set_with_dataframe
-import requests
-
+import aiohttp
+import asyncio
 
 init(autoreset=True)
 load_dotenv()
 
 start_time = time.time()
 
-start_date = datetime(2024, 5, 1)
-end_date = datetime.now()
-start_date_str = start_date.strftime('%Y-%m-%d')
-end_date_str = end_date.strftime('%Y-%m-%d')
-time_range = json.dumps({'since': start_date_str, 'until': end_date_str})
+async def get_date_range():
+    start_date = datetime(2024, 4, 1)
+    end_date = datetime.now()
+    start_date_str = start_date.strftime('%Y-%m-%d')
+    end_date_str = end_date.strftime('%Y-%m-%d')
+    return json.dumps({'since': start_date_str, 'until': end_date_str})
 
-def google_sheet_import(fb_ads_data, sheet_name):
+async def google_sheet_import(fb_ads_data, sheet_name):
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name('fb-ads-data-423818-c13708cd5253.json', scope)
     client = gspread.authorize(creds)
@@ -40,62 +41,104 @@ def google_sheet_import(fb_ads_data, sheet_name):
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
-def fetch_meta_insight_data(access_token, account_id):
+def extract_action(action_list, action_type):
+    for action in action_list:
+        if action['action_type'] == action_type:
+            return action['value']
+    return 0
+
+async def fetch_meta(access_token, account_id):
     data = []
-    start_date = datetime(2024, 1, 1)
-    end_date = datetime.now()
-    start_date_str = start_date.strftime('%Y-%m-%d')
-    end_date_str = end_date.strftime('%Y-%m-%d')
-    time_range = json.dumps({'since': start_date_str, 'until': end_date_str})
+    time_range = await get_date_range()
 
     url = f'https://graph.facebook.com/v20.0/act_{account_id}/insights'
     params = {
         'access_token': access_token,
         'time_range': time_range,
-        'fields': 'account_currency, campaign_name, adset_name, ad_name, impressions, clicks, spend, reach, actions, objective, outbound_clicks',
+        'fields': 'account_currency, campaign_id, campaign_name, impressions, clicks, spend, reach, actions, objective',
         'time_increment': 1,
-        'level': {'ad', 'adset', 'campaign', 'account'}        
+        'level': 'campaign'
     }
 
-    while url:
-        response = requests.get(url, params=params)
-        if response.status_code == 200:
-            response_json = response.json()
-            data.extend(response_json['data'])
-            url = response_json.get('paging', {}).get('next')
-        else:
-            print(f"Error: {response.status_code}")
-            print(response.text)
-            break
+    async with aiohttp.ClientSession() as session:
+        while url:
+            async with session.get(url, params=params) as response:
+                if response.status == 200:
+                    response_json = await response.json()
+                    data.extend(response_json['data'])
+                    url = response_json.get('paging', {}).get('next')
+                else:
+                    print(f"Error: {response.status}")
+                    print(await response.text())
+                    break
 
     fb_ads_data = pd.DataFrame(data)
 
-    # Parse actions field to extract link_click
-    def extract_action(action_list, action_type):
-        for action in action_list:
-            if action['action_type'] == action_type:
-                return action['value']
-        return 0
+    columns = ['date_start', 'account_currency', 'campaign_name', 'campaign_id', 'impressions', 'clicks', 'spend', 'reach', 'actions', 'objective']
+    for column in columns:
+        if column not in fb_ads_data.columns:
+            fb_ads_data[column] = None
 
-    fb_ads_data['link_click'] = fb_ads_data['actions'].apply(lambda x: extract_action(x, 'link_click') if isinstance(x, list) else 0)
-    fb_ads_data['comments'] = fb_ads_data['actions'].apply(lambda x: extract_action(x, 'comment') if isinstance(x, list) else 0)
-    fb_ads_data['leads'] = fb_ads_data['actions'].apply(lambda x: extract_action(x, 'lead') if isinstance(x, list) else 0)
-    fb_ads_data['outbound_clicks'] = fb_ads_data['outbound_clicks'].apply(lambda x: extract_action(x, 'outbound_click') if isinstance(x, list) else 0)
-    
-    fb_ads_data = fb_ads_data.rename(columns={'date_start': 'date'})
-    fb_ads_data = fb_ads_data.sort_values(by='date', ascending=False)
-    fb_ads_data = fb_ads_data[['date', 'account_currency', 'campaign_name', 'adset_name', 'ad_name','impressions', 'clicks', 'spend', 'reach', 'link_click', 'comments', 'leads', 'objective', 'outbound_clicks']]
-    fb_ads_data.to_csv('test_Data.csv')
+    action_columns = ['link_clicks', 'video_views', 'post_engagements', 'post_reactions']
+    action_types = ['link_click', 'video_view', 'post_engagement', 'post_reaction']
+    for action_col, action_type in zip(action_columns, action_types):
+        fb_ads_data[action_col] = fb_ads_data['actions'].apply(lambda x: extract_action(x, action_type) if isinstance(x, list) else 0)
+
+    fb_ads_data = fb_ads_data[['date_start', 'account_currency', 'campaign_id', 'campaign_name', 'impressions', 'clicks', 'spend', 'reach', 'link_clicks', 'video_views', 'post_engagements', 'post_reactions', 'objective']]
+
     return fb_ads_data
 
-def main():
+async def fetch_adset_ad(session, access_token, account_id, level, id, id2, name):
+    data = []
+    time_range = await get_date_range()
+
+    url = f'https://graph.facebook.com/v20.0/act_{account_id}/insights'
+    params = {
+        'access_token': access_token,
+        'time_range': time_range,
+        'fields': f"{id}, {id2} ,{name}",
+        'time_increment': 1,
+        'level': level
+    }
+
+    while url:
+        async with session.get(url, params=params) as response:
+            if response.status == 200:
+                response_json = await response.json()
+                data.extend(response_json['data'])
+                url = response_json.get('paging', {}).get('next')
+            else:
+                print(f"Error: {response.status}")
+                print(await response.text())
+                break
+
+    df = pd.DataFrame(data)
+    return df
+
+async def merge_data(adset_data, ad_data, df):
+    df1 = pd.merge(adset_data, ad_data, on=['adset_id', 'date_start', 'date_stop'], how='outer')
+    df2 = df1[['campaign_id', 'adset_name', 'ad_name', 'date_start']]
+    data = pd.merge(df, df2, on=['campaign_id', 'date_start'], how='outer')
+    final_data = data[['date_start', 'account_currency', 'campaign_name', 'adset_name', 'ad_name', 'impressions', 'clicks', 'spend', 'reach', 'link_clicks', 'video_views', 'post_engagements', 'post_reactions', 'objective']]
+    final_data = final_data.drop_duplicates(subset='impressions')
+    final_data = final_data.sort_values(by='date_start', ascending=False)
+    final_data.to_csv('test.csv')
+    return final_data
+
+async def main():
     access_token = os.getenv("fb_access_token")
-    account_ids = [os.getenv('account_id_2'), os.getenv('account_id_4'), os.getenv('account_id_1')]
-    for account_id in account_ids:
-        data = fetch_meta_insight_data(access_token=access_token, account_id=account_id)
-        print(data)
-        sheet_name = f"meta_{account_id}"
-        google_sheet_import(data, sheet_name)
+    account_ids = [os.getenv('account_id_1'), os.getenv('account_id_2'), os.getenv('account_id_3'), os.getenv('account_id_4')]
+
+    async with aiohttp.ClientSession() as session:
+        for account_id in account_ids:
+            df = await fetch_meta(access_token=access_token, account_id=account_id)
+            adset_data = await fetch_adset_ad(session, access_token, account_id, level='adset', id='adset_id', id2='campaign_id', name='adset_name')
+            ad_data = await fetch_adset_ad(session, access_token, account_id, level='ad', id='ad_id', id2='adset_id', name='ad_name')
+
+            data = await merge_data(adset_data=adset_data, ad_data=ad_data, df=df)
+            print(data)
+            sheet_name = f"meta_{account_id}"
+            await google_sheet_import(data, sheet_name)
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
